b96aa2010dae87d26b3119c32cd4695b55e3fbf5
Jessie Newman <Jessie-Newman@users.noreply.github.com>
2024-01-13 13:11:29 -0500

    Add unlimited play behind comment

--- content-script.js
+++ content-script.js
@@ -2,5 +2,28 @@ window.addEventListener('load', function () {
   rollAgain = document.getElementsByTagName('input')[13]
   if (rollAgain.value == 'Roll Again') {
     rollAgain.click()
+    return
+  } 
+
+  // Could not find Roll Again button. Look for Press Me.
+  pressMe = document.getElementsByTagName('input')[11]
+  if (pressMe.value == 'Press Me') {
+    pressMe.click()
+    return
   }
+
+  // Could not find Roll Again or Press Me button. Look for Lets Play.
+  letsPlay = document.getElementsByTagName('input')[13]
+  if (letsPlay.value == 'Lets Play! (Costs 5 NP)') {
+    letsPlay.click()
+    return
+  }
+
+  // Uncomment this section for infinite play.
+
+  // playDiceARoo = document.getElementsByTagName('input')[14]
+  // if (playDiceARoo.value == 'Play Dice-A-Roo') {
+  //   playDiceARoo.click()
+  //   return
+  // }
 })
