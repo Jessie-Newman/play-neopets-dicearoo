f540f399ed781e836ba7964e6b1bf93a42111b3d
Jessie Newman <Jessie-Newman@users.noreply.github.com>
2024-02-21 19:41:38 +0000

    Refresh on page errors.

--- content-script.js
+++ content-script.js
@@ -1,29 +1,43 @@
+TIMEOUT_MIN = 500
+TIMEOUT_MAX = 2000
+
 window.addEventListener('load', function () {
-  rollAgain = document.getElementsByTagName('input')[13]
-  if (rollAgain.value == 'Roll Again') {
-    rollAgain.click()
-    return
-  } 
+  setTimeout(() => {
+    errors = document.getElementsByTagName('h1')
+    if (errors.length > 0) {
+      error = errors[0]
+      if (error.value == 'Service Unavailable - Zero size object') {
+        this.window.location.reload()
+      }
+    }
 
-  // Could not find Roll Again button. Look for Press Me.
-  pressMe = document.getElementsByTagName('input')[11]
-  if (pressMe.value == 'Press Me') {
-    pressMe.click()
-    return
-  }
+    rollAgain = document.getElementsByTagName('input')[13]
+    if (rollAgain.value == 'Roll Again') {
+      rollAgain.click()
+      return
+    } 
 
-  // Could not find Roll Again or Press Me button. Look for Lets Play.
-  letsPlay = document.getElementsByTagName('input')[13]
-  if (letsPlay.value == 'Lets Play! (Costs 5 NP)') {
-    letsPlay.click()
-    return
-  }
+    // Could not find Roll Again button. Look for Press Me.
+    pressMe = document.getElementsByTagName('input')[11]
+    if (pressMe.value == 'Press Me') {
+      pressMe.click()
+      return
+    }
 
-  // Uncomment this section for infinite play.
+    // Could not find Roll Again or Press Me button. Look for Lets Play.
+    letsPlay = document.getElementsByTagName('input')[13]
+    if (letsPlay.value == 'Lets Play! (Costs 5 NP)') {
+      letsPlay.click()
+      return
+    }
 
-  // playDiceARoo = document.getElementsByTagName('input')[14]
-  // if (playDiceARoo.value == 'Play Dice-A-Roo') {
-  //   playDiceARoo.click()
-  //   return
-  // }
+    // Uncomment this section for infinite play.
+
+    playDiceARoo = document.getElementsByTagName('input')[14]
+    if (playDiceARoo.value == 'Play Dice-A-Roo') {
+      playDiceARoo.click()
+      return
+    }
+  }, TIMEOUT_MIN + (Math.random() * TIMEOUT_MAX));
 })
+
