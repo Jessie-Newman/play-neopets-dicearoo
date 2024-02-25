60a844e495ddbe67227ceabee035dad2ce09d0b9
Jessie Newman <Jessie-Newman@users.noreply.github.com>
2024-02-24 23:02:37 -0500

    Handle more errors & jackpot

--- content-script.js
+++ content-script.js
@@ -1,12 +1,13 @@
 TIMEOUT_MIN = 500
 TIMEOUT_MAX = 2000
+EXPECTED_ERRORS = ['Service Unavailable - Zero size object', 'Internal Server Error - Read']
 
 window.addEventListener('load', function () {
   setTimeout(() => {
     errors = document.getElementsByTagName('h1')
     if (errors.length > 0) {
       error = errors[0]
-      if (error.textContent == 'Service Unavailable - Zero size object') {
+      if (EXPECTED_ERRORS.includes(error.textContent)) {
         this.window.location.reload()
       }
     }
@@ -28,6 +29,13 @@ window.addEventListener('load', function () {
     letsPlay = document.getElementsByTagName('input')[13]
     if (letsPlay.value == 'Lets Play! (Costs 5 NP)') {
       letsPlay.click()
+      return
+    }
+
+    // Could not find Roll Again, Press Me, or Lets Play button. Look for Play Again.
+    playAgain = document.getElementsByTagName('input')[11]
+    if (playAgain.value == 'Play Again!') {
+      playAgain.click()
       return
     }
 
